c6ae47bbe93a5ad71233318e5f147d55bbf6ca13
Kevin Lee <kleekev@sas.upenn.edu>
2023-09-09 16:05:55 -0400

    Added signup button functionality

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -3,6 +3,8 @@ import { Select } from 'antd';
 
 
 const SignUp = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
@@ -13,6 +15,30 @@ const SignUp = () => {
         setFluentLanguage(value)
     };
 
+    const handleSubmit = async () => {
+        setIsLoading(true);
+        const response = await fetch('http://localhost:4000/users/signup', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({username, email, password, fluentLanguage, learningLanguage})
+        });
+
+        const json = await response.json();
+
+        if (!response.ok) {
+            setIsLoading(false);
+            setError(json.error);
+        }
+
+        if (response.ok) {
+            // save the user to local storage
+            localStorage.setItem('user', JSON.stringify(json)); 
+
+            setIsLoading(false);
+        }
+    }
+
+    
     const options = [
         {
             value: 'Afrikaans',
@@ -26,7 +52,7 @@ const SignUp = () => {
 
     return (
         <div className="signup-page">
-            <form className='signup-form' action="">
+            <form className='signup-form' onSubmit={handleSubmit}>
                 <h1>Sign Up!</h1>
                 <label>Username:</label>
                 <input 
@@ -48,6 +74,7 @@ const SignUp = () => {
                     onChange={(e) => setPassword(e.target.value)}
                     value={password}
                 />
+                <label>Select languages you are fluent in</label>
                 <Select
                     showSearch
                     mode="tags"
@@ -56,6 +83,8 @@ const SignUp = () => {
                     style={{ width: '100%' }}
                     options={options}
                 />
+                <button disabled={isLoading}>Sign up</button>
+
             </form>
         </div>
     )
